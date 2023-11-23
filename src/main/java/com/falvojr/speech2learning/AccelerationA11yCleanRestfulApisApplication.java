0a2aaf13f6613e61602742007d177105d6b1eceb
Venilton FalvoJr <falvojr@gmail.com>
2023-11-22 22:10:34 -0300

    First commit (spring initializr)

---
+++ src/main/java/com/falvojr/speech2learning/AccelerationA11yCleanRestfulApisApplication.java
@@ -0,0 +1,13 @@
+package com.falvojr.speech2learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccelerationA11yCleanRestfulApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccelerationA11yCleanRestfulApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falvojr/speech2learning/AccelerationA11yCleanRestfulApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falvojr.speech2learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccelerationA11yCleanRestfulApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
